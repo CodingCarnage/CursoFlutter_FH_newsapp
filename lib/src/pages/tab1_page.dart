778df63205ce66e65c8fb3dcad09b1f32e6d33ce
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-23 17:27:46 -0600

    Se agregaron elementos a la pagina.

---
+++ lib/src/pages/tab1_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import 'package:newsapp/src/services/news_service.dart';
+
+class Tab1Page extends StatelessWidget {
+  const Tab1Page({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final NewsService newsService = Provider.of<NewsService>(context);
+
+    return Scaffold(
+      body: Center(
+        child: Text('Tab1 Page'),
+      ),
+    );
+  }
+}
