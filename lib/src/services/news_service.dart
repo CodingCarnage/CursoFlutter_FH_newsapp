14591808173cefbffd0a7d862371d81cc487d87f
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-23 17:28:41 -0600

    Se hace un request para obtener los encabezados de las noticias.

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 import 'package:newsapp/src/models/news_models.dart';
+
+final _URL_NEWSAPI = 'https://newsapi.org/v2';
+final _APIKEY = '9e775723f8ce4846b31548faac0703b1';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
@@ -9,7 +14,13 @@ class NewsService with ChangeNotifier {
     this.getTopHeadlines();
   }
 
-  getTopHeadlines() {
-    print('Loading Headlines...');
+  getTopHeadlines() async {
+    final String url = '$_URL_NEWSAPI/top-headlines?apiKey=$_APIKEY&country=mx';
+    final http.Response response = await http.get(url);
+
+    final NewsResponse newsResponse = newsResponseFromJson(response.body);
+
+    this.headlines.addAll(newsResponse.articles);
+    notifyListeners();
   }
 }
