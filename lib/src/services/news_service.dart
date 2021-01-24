6ecc55a9e0e85d81dfeb88ed9b11e5add4aa8949
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-24 12:56:18 -0600

    Se crearon diferentes categorias en el servicio.

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import 'package:newsapp/src/models/category_model.dart';
+export 'package:newsapp/src/models/category_model.dart';
 import 'package:newsapp/src/models/news_models.dart';
 
 const _URL_NEWSAPI = 'https://newsapi.org/v2';
@@ -9,6 +12,16 @@ const _APIKEY = '9e775723f8ce4846b31548faac0703b1';
 
 class NewsService with ChangeNotifier {
   List<Article> headlines = [];
+
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'business'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
 
   NewsService () {
     this.getTopHeadlines();
