7ccf547d78125c549efd84d9f53bb4224c5c6e55
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-24 12:26:06 -0600

    Cambio de color en un boton.

--- lib/src/widgets/list_news_widget.dart
+++ lib/src/widgets/list_news_widget.dart
@@ -174,7 +174,7 @@ class _CardButtons extends StatelessWidget {
           const SizedBox(width: 10.0),
           RawMaterialButton(
             onPressed: () {},
-            fillColor: darkTheme.accentColor,
+            fillColor: Colors.blue,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20.0),
             ),
