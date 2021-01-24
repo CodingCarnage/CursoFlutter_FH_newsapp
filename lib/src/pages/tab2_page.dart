03145d4f5c336b1a8de95d794a7c0c9745e0414d
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-24 14:11:20 -0600

    Se pueden observar los articulos de acuerdo a la categoria seleccionada.

--- lib/src/pages/tab2_page.dart
+++ lib/src/pages/tab2_page.dart
@@ -6,21 +6,32 @@ import 'package:newsapp/src/services/news_service.dart';
 
 import 'package:newsapp/src/theme/dark_theme.dart';
 
+import 'package:newsapp/src/widgets/list_news_widget.dart';
+
 class Tab2Page extends StatelessWidget {
   const Tab2Page({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final List<Category> categories =
-        Provider.of<NewsService>(context).categories;
+    final NewsService newsService = Provider.of<NewsService>(context);
 
     return SafeArea(
       child: Scaffold(
         body: Column(
           children: <Widget>[
-            Expanded(
-              child: _ListCategories(categories: categories),
-            ),
+            _ListCategories(),
+            if (!newsService.isLoading)
+              Expanded(
+                child: ListNews(
+                  articles: newsService.articleFromSelectedCategory,
+                ),
+              ),
+            if (newsService.isLoading)
+              Expanded(
+                child: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
           ],
         ),
       ),
@@ -31,29 +42,34 @@ class Tab2Page extends StatelessWidget {
 class _ListCategories extends StatelessWidget {
   const _ListCategories({
     Key key,
-    @required this.categories,
   }) : super(key: key);
-
-  final List<Category> categories;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      itemCount: categories.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: <Widget>[
-              _CategoryButton(category: categories[index]),
-              const SizedBox(height: 5.0),
-              Text('${categories[index].name[0].toUpperCase()}${categories[index].name.substring(1)}'),
-            ],
-          ),
-        );
-      },
+    final List<Category> categories =
+        Provider.of<NewsService>(context).categories;
+
+    return Container(
+      width: double.infinity,
+      height: 80.0,
+      child: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                _CategoryButton(category: categories[index]),
+                const SizedBox(height: 5.0),
+                Text(
+                    '${categories[index].name[0].toUpperCase()}${categories[index].name.substring(1)}'),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
@@ -69,10 +85,11 @@ class _CategoryButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final NewsService newsService = Provider.of<NewsService>(context);
-    
+
     return GestureDetector(
       onTap: () {
-        final NewsService newsService = Provider.of<NewsService>(context, listen: false);
+        final NewsService newsService =
+            Provider.of<NewsService>(context, listen: false);
         newsService.selectedCategory = category.name;
       },
       child: Container(
@@ -85,7 +102,9 @@ class _CategoryButton extends StatelessWidget {
         ),
         child: Icon(
           category.icon,
-          color: (newsService.selectedCategory != category.name) ? Colors.black54 : darkTheme.accentColor,
+          color: (newsService.selectedCategory != category.name)
+              ? Colors.black54
+              : darkTheme.accentColor,
         ),
       ),
     );
