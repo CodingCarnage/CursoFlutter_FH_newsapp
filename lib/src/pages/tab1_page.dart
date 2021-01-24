2f172426c862cd9bf8d54adb9148b8611fcc4697
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2021-01-24 12:27:41 -0600

    Se cambio a un StatefulWidget para mantener el estado del scroll al cambiar de pantalla.

--- lib/src/pages/tab1_page.dart
+++ lib/src/pages/tab1_page.dart
@@ -6,17 +6,25 @@ import 'package:newsapp/src/services/news_service.dart';
 
 import 'package:newsapp/src/widgets/list_news_widget.dart';
 
-class Tab1Page extends StatelessWidget {
+class Tab1Page extends StatefulWidget {
   const Tab1Page({Key key}) : super(key: key);
 
+  @override
+  _Tab1PageState createState() => _Tab1PageState();
+}
+
+class _Tab1PageState extends State<Tab1Page> with AutomaticKeepAliveClientMixin {
   @override
   Widget build(BuildContext context) {
     final NewsService newsService = Provider.of<NewsService>(context);
 
     return Scaffold(
-      body: ListNews(
-        articles: newsService.headlines,
-      ),
+      body: (newsService.headlines.length == 0)
+          ? Center(child: CircularProgressIndicator())
+          : ListNews(articles: newsService.headlines),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
